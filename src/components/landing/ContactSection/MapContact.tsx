2e8e96339bc5fb6fd2cc7364202fa53324bd2885
JunaiCode <juanpalta24@hotmail.com>
2025-06-21 12:00:50 -0500

    edit map

--- src/components/landing/ContactSection/MapContact.tsx
+++ src/components/landing/ContactSection/MapContact.tsx
@@ -4,7 +4,7 @@ export const MapContact = ()=>{
        <div className="w-full h-96">
             <iframe
               title="Pet Store Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.797519624211!2d-75.57024232580799!3d6.249662293738575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e4429a840dfef69%3A0x2d6cb1717cb52c9a!2sMedell%C3%ADn%2C%20Antioquia!5e0!3m2!1sen!2sco!4v1718804823603!5m2!1sen!2sco"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.4569146217843!2d-76.53370257583933!3d3.3418948966317994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e30a1eb8d09a3c1%3A0x766b9b441dfa8bbe!2sUniversidad%20Icesi!5e0!3m2!1ses-419!2sco!4v1718990844302!5m2!1ses-419!2sco"
               width="100%"
               height="100%"
               style={{ border: 0 }}
