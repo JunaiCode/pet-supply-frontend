991a060af0e7189c2059fd7cf923bffdbde33788
JunaiCode <juanpalta24@hotmail.com>
2025-08-17 12:25:18 -0500

    featured products map

--- src/components/landing/FeaturedSection/FeaturedCarrousel.tsx
+++ src/components/landing/FeaturedSection/FeaturedCarrousel.tsx
@@ -5,10 +5,9 @@ import { useMemo } from "react";
 import { FeaturedProductsCard } from "./FeaturedProductsCard";
 
 export const FeaturedProductsCarousel = () => {
-  
   const filters = useMemo(() => ({ limit: 5, availability: "true" }), []);
   const { products, loading } = useFetchProducts(filters);
-  
+
   if (loading) {
     return (
       <p className="text-center text-gray-500 py-4">
@@ -21,9 +20,11 @@ export const FeaturedProductsCarousel = () => {
     <div>
       <div className="relative w-full overflow-hidden">
         <div className="animate-marquee whitespace-nowrap flex gap-6">
-          {products.map(product => (
-            // Replace the following div with your actual product card/component
-           <FeaturedProductsCard key={product.id} product={product}/>
+          {[...products, ...products].map((product, idx) => (
+            <FeaturedProductsCard
+              key={`${idx}-${product.id}`}
+              product={product}
+            />
           ))}
         </div>
       </div>
